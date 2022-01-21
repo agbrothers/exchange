Clean up repo, implement normalization, fix bugs

envs/exchange.py
@@ -1,57 +1,60 @@
 import yaml
 import numpy as np
+import gym
+from gym.spaces import Box
 
-from envs.base_env import BaseEnv
-from envs.market import Market
-from envs.trader import Trader
-from envs.portfolio import Portfolio
+from base_env import BaseEnv
+from market import Market
+from portfolio import Portfolio
 
 
-class Exchange(BaseEnv):
+class Exchange(gym.Env):
 
     # NOTE: 
     # This environment is open-loop and assumes the quantities traded will not materially impact prices
 
     def __init__(self, env_config:dict):
-        self.market = Market(env_config)        # Environment 
-        self.portfolio = Portfolio(env_config)  # Reward Function
-        self.trader = Trader(env_config)                  # Agent
+        self.market = Market(env_config)        # ~Environment 
+        self.portfolio = Portfolio(env_config)  # ~Reward Function
 
         self.state = None
         self.current_step = None
+        self.norm_constants=None
 
-        self.reset()
+        ics = self.reset()
         self.max_steps = self.market.max_steps
+        self._action_space = Box(-1, 1, shape=[len(env_config["tickers"])]) # sell -1 > 0, hold == 0, buy 0 < 1        
+        self._observation_space = ics.shape     # np.array(2 + 3*self.num_stocks + 28*self.num_stocks)
 
 
     def reset(self):
         self.current_step = 0
-        self.trader.reset()
         market_ics = self.market.reset()
-        portfolio_ics = self.portfolio.reset()
-        self.state = np.array(portfolio_ics + market_ics,  dtype=np.float32)
-        return self.state
+        portfolio_ics = self.portfolio.reset(market_ics)
+        self.norm_constants = np.concatenate((self.portfolio.norm_constants, self.market.norm_constants))
+        self.state = np.concatenate((portfolio_ics, market_ics))
+        return self.state/self.norm_constants
 
 
     def step(self, action):
         # Add N time step lag for buy/sell orders
         self.current_step += 1
-        trades = self.trader.step(action)
         market_obs, done = self.market.step()
-        portfolio_obs, reward = self.portfolio.step(trades, market_obs)
-        self.state = np.array(portfolio_obs + market_obs,  dtype=np.float32)
-        return self.state, reward, done
+        portfolio_obs, reward = self.portfolio.step(action, self.market.stocks)
+        self.state = np.concatenate((portfolio_obs, market_obs))
+        return self.state/self.norm_constants, reward, done
 
 
     def action_space(self):
-        pass
+        return self._action_space
     def observation_space(self):
-        pass
+        return self._observation_space
     def close(self):
         pass
     def render(self):
         pass
-
+    def seed(self):
+        pass
 
 
 if __name__ == "__main__":
@@ -68,6 +71,7 @@ if __name__ == "__main__":
         ],
     }
     env = Exchange(env_config)
+    env.step(np.array([0.5,0.3]))
 
 
 

envs/market.py
@@ -6,10 +6,10 @@ import pandas as pd
 from gym.spaces import Box
 
 from base_env import BaseEnv
-from envs.preprocessors import StockPreprocessor
+from preprocessors import StockPreprocessor
 
 
-class Market(BaseEnv):
+class Market():
 
     # NOTE: 
     # This environment is open-loop and assumes the quantities traded will not materially impact the price
@@ -25,8 +25,8 @@ class Market(BaseEnv):
         self.order_lag = env_config["order_lag"] # Lag in minutes for buy/sell orders to execute
 
         self.load_data()
-        self.max_steps = self.stocks[self.tickers[0]]["data"].shape[0] // self.dt # Number of minutes from market open to close
-        
+        self.max_steps = self.stocks[self.tickers[0]]["historical_data"].shape[0] // self.dt # Number of minutes from market open to close
+        self.norm_constants=np.array([])
 
     def load_data(self):
         ticker_paths = {
@@ -60,11 +60,11 @@ class Market(BaseEnv):
         initial_conditions = []
 
         for ticker,stock in self.stocks.items():
-            self.stocks[ticker]["live_data"] = stock["data"].groupby("date").get_group(self.date)[self.features].values
-            self.stocks[ticker]["current_price"] = self.stocks[ticker]["live_data"][0]            
-            self.stocks[ticker]["preprocessor"] = StockPreprocessor(self.stocks[ticker]["current_price"], self.dt)
-            initial_conditions.append(self.stocks[ticker]["current_price"])
-        
+            self.stocks[ticker]["live_data"] = stock["historical_data"].groupby("date").get_group(self.date)[self.features].values
+            self.stocks[ticker]["current_price"] = list(self.stocks[ticker]["live_data"][0])            
+            self.stocks[ticker]["preprocessor"] = StockPreprocessor(self.stocks[ticker].get("current_price"), self.dt)
+            initial_conditions += list(self.stocks[ticker]["preprocessor"].get_state())  #self.stocks[ticker].get("current_price")
+            self.norm_constants = np.append(self.norm_constants, self.stocks[ticker]["preprocessor"].norm_constants)
         return initial_conditions
 
 
@@ -74,9 +74,9 @@ class Market(BaseEnv):
         obs = []
 
         for ticker in self.stocks:
-            self.stocks[ticker]["current_price"] = self.stocks[ticker]["live_data"][self.current_step * self.dt]            
-            self.stocks[ticker]["preprocessor"].update(self.stocks[ticker]["current_price"])
-            obs.append(self.stocks[ticker]["preprocessor"].get_state())
+            self.stocks[ticker]["current_price"] = self.stocks[ticker].get("live_data")[self.current_step * self.dt]            
+            self.stocks[ticker]["preprocessor"].update(self.stocks[ticker].get("current_price"))
+            obs+= list(self.stocks[ticker]["preprocessor"].get_state())
         done = self.current_step==self.max_steps-1
         return obs, done
 

envs/portfolio.py
@@ -1,50 +1,75 @@
+import numpy as np
+
 
 class Portfolio:  # properties and state of physical world entity
     def __init__(self, env_config):
-
-        self.name = ""
         self.starting_balance = env_config["starting_balance"]
         self.tickers = env_config["tickers"]
-        # state
-        # self.state = EntityState()
+        self.norm_constants = None
 
 
-    def reset(self):
+    def reset(self, ics):
         self.cash_balance = self.starting_balance
         self.portfolio_value = self.starting_balance
-        self.num_positions = 0
+        self.norm_constants = [self.starting_balance, self.starting_balance]
+        # reset trading positions and normalization constants
         self.positions = {}
-        for ticker in self.tickers:
+        for i,ticker in enumerate(self.tickers):
             self.positions[ticker] = {
                 "num_shares":0,
+                "price_per_share": ics[7 + i*28],
                 "value":0,
                 "portfolio_diversity":0,
             }
+            self.norm_constants.append( self.starting_balance / self.positions[ticker].get("price_per_share") )
+            self.norm_constants.append( self.starting_balance )
+            self.norm_constants.append( 1.0 )
+
+        self.norm_constants = np.array(self.norm_constants)
+        portfolio_ics = np.array([self.portfolio_value, self.cash_balance] + [0] * 3 * len(self.tickers), dtype=np.float32)
+        return portfolio_ics
 
 
-    def step(self, trades, market_obs):    
-        for trade in trades:
-            self.portfolio[trade["ticker"]] += trade.num_shares
-            self.cash_balance -= trade.value
+    def step(self, action, market_obs): 
+        # CONVERT ACTIONS TO TRADES   
+        for i,trade in enumerate(action):
+            ticker = self.tickers[i]
+            if trade == 0: # Hold
+                continue
+            elif trade > 0: # Buy: action * remaining_cash_bal worth of shares
+                buy_order_value = trade * self.cash_balance 
+                buy_order_shares = (buy_order_value / self.positions[ticker].get("price_per_share"))
+                self.cash_balance -= buy_order_value
+                self.positions[ticker]["num_shares"] += buy_order_shares
+            else: # Sell: action * stock_value worth of shares
+                sell_order_value = trade * self.positions[ticker]["value"]
+                sell_order_shares = trade * self.positions[ticker]["num_shares"]
+                self.cash_balance -= sell_order_value
+                self.positions[ticker]["num_shares"] += sell_order_shares                
 
+        # UPDATE VALUE OF POSITIONS
         self.prev_portfolio_value = self.portfolio_value
-        self.portfolio_value = 0
+        self.portfolio_value = self.cash_balance
         for ticker in self.positions:
-            price_per_share = market_obs[ticker]["current_price"][3] # 3 ~ step close price
-            self.position[ticker]["value"] = price_per_share * self.position[ticker]["num_shares"] 
-            self.portfolio_value += self.position[ticker]["value"]
+            price_per_share = market_obs[ticker].get("current_price")[3] # 3 ~ step close price
+            self.positions[ticker]["value"] = price_per_share * self.positions[ticker].get("num_shares")
+            self.positions[ticker]["price_per_share"] = price_per_share
+            self.portfolio_value += self.positions[ticker].get("value")
 
         portfolio_obs = [self.portfolio_value, self.cash_balance]
         
+        # UPDATE PORTFOLIO DIVERSITY and OBS
         for ticker in self.positions:
-            self.position[ticker]["portfolio_diversity"] = self.position[ticker]["value"] / self.portfolio_value
-            portfolio_obs.append([
-                self.position[ticker]["num_shares"],
-                self.position[ticker]["value"], 
-                self.position[ticker]["portfolio_diversity"]
-            ])
+            self.positions[ticker]["portfolio_diversity"] = self.positions[ticker]["value"] / self.portfolio_value
+            portfolio_obs += [
+                self.positions[ticker]["num_shares"],
+                self.positions[ticker]["value"], 
+                self.positions[ticker]["portfolio_diversity"]
+            ]
+        portfolio_obs
+
+        # COMPUTE REWARD
         step_percentage_change = 1 - (self.portfolio_value / self.prev_portfolio_value)
         day_percentage_change = 1 - (self.portfolio_value / self.starting_balance)   
         reward = step_percentage_change + 0.1 * day_percentage_change
-        
         return portfolio_obs, reward

envs/preprocessors.py
@@ -7,9 +7,10 @@ class StockPreprocessor:
 
     def __init__(self, ics, dt=1):
         self.ticker = ""
-        self.obs = {}
+        self.obs_dict = {}
         self.timestep = -1 
         self.dt = dt    
+        self.norm_constants=None
 
         # Histories
         self.price_history = []
@@ -18,14 +19,15 @@ class StockPreprocessor:
         self.volume_percentage_history = []
 
         # Intraday price high
-        self.obs["day_price_high"] = 0
+        self.obs_dict["day_price_high"] = 0
         # Intraday price low
-        self.obs["day_price_low"] = np.inf
+        self.obs_dict["day_price_low"] = np.inf
 
         # Intraday volume high
-        self.obs["day_volume_high"] = 0
+        self.obs_dict["day_volume_high"] = 0
         # Intraday low
-        self.obs["day_volume_low"] = np.inf
+        self.obs_dict["day_volume_low"] = np.inf
+
 
         self.update(ics)
 
@@ -34,76 +36,80 @@ class StockPreprocessor:
         self.timestep += 1
 
         # current time interval share price open
-        self.obs["current_price_open"] = obs[0]
+        self.obs_dict["current_price_open"] = obs[0]
         # current time interval share price high
-        self.obs["current_price_high"] = obs[1]
+        self.obs_dict["current_price_high"] = obs[1]
         # current time interval share price low
-        self.obs["current_price_low"] = obs[2]
+        self.obs_dict["current_price_low"] = obs[2]
         # current time interval share price close
-        self.obs["current_price_close"] = obs[3]
+        self.obs_dict["current_price_close"] = obs[3]
         # current time interval share volume
-        self.obs["current_volume"] = obs[4]
+        self.obs_dict["current_volume"] = obs[4]
 
         # current price percentage change
-        self.obs["current_price_percentage_change"] = self.obs["current_price_close"] / self.obs["current_price_open"] - 1
+        self.obs_dict["current_price_percentage_change"] = self.obs_dict["current_price_close"] / self.obs_dict["current_price_open"] - 1
         # current price percentage high
-        self.obs["current_price_percentage_high"]   = self.obs["current_price_close"] / self.obs["current_price_high"] - 1
+        self.obs_dict["current_price_percentage_high"] = self.obs_dict["current_price_close"] / self.obs_dict["current_price_high"] - 1
         # current price percentage low
-        self.obs["current_price_percentage_low"]    = self.obs["current_price_close"] / self.obs["current_price_low"] - 1
+        self.obs_dict["current_price_percentage_low"] = self.obs_dict["current_price_close"] / self.obs_dict["current_price_low"] - 1
 
-        self.price_history.append(self.obs["current_price_close"])
-        self.price_percentage_history.append(self.obs["current_price_percentage_change"])
+        self.price_history.append(self.obs_dict["current_price_close"])
+        self.price_percentage_history.append(self.obs_dict["current_price_percentage_change"])
 
         # Intraday price open
-        if self.timestep==0: self.obs["day_price_open"] = self.obs["current_price_open"]
+        if self.timestep==0: self.obs_dict["day_price_open"] = self.obs_dict["current_price_open"]
         # Intraday price high
-        self.obs["day_price_high"]    = max(self.obs["current_price_high"], self.obs["day_price_high"])
+        self.obs_dict["day_price_high"] = max(self.obs_dict["current_price_high"], self.obs_dict["day_price_high"])
         # Intraday price low
-        self.obs["day_price_low"]     = min(self.obs["current_price_low"], self.obs["day_price_low"])
+        self.obs_dict["day_price_low"] = min(self.obs_dict["current_price_low"], self.obs_dict["day_price_low"])
         # Intraday price avg
-        self.obs["day_price_median"]  = np.median(self.price_history[::self.dt])
+        self.obs_dict["day_price_median"] = np.median(self.price_history[::self.dt])
         # Intraday price 30m moving avg
-        self.obs["day_price_30m_avg"] = np.mean(self.price_history[-30::self.dt])
+        self.obs_dict["day_price_30m_avg"] = np.mean(self.price_history[-30::self.dt])
 
         # Intraday price percentage change
-        self.obs["day_price_percentage_change"]  = self.obs["current_price_close"] / self.obs["day_price_open"] - 1
+        self.obs_dict["day_price_percentage_change"] = self.obs_dict["current_price_close"] / self.obs_dict["day_price_open"] - 1
         # Intraday price percentage high
-        self.obs["day_price_percentage_high"]    = self.obs["current_price_close"] / self.obs["day_price_high"] - 1
+        self.obs_dict["day_price_percentage_high"] = self.obs_dict["current_price_close"] / self.obs_dict["day_price_high"] - 1
         # Intraday price percentage low
-        self.obs["day_price_percentage_low"]     = self.obs["current_price_close"] / self.obs["day_price_low"] - 1
+        self.obs_dict["day_price_percentage_low"] = self.obs_dict["current_price_close"] / self.obs_dict["day_price_low"] - 1
         # Intraday price avg
-        self.obs["day_price_median_percentage"]  = np.median(self.price_percentage_history[::self.dt])
+        self.obs_dict["day_price_median_percentage"] = np.median(self.price_percentage_history[::self.dt])
         # Intraday price 30m moving avg
-        self.obs["day_price_30m_avg_percentage"] = np.mean(self.price_percentage_history[-30::self.dt])
+        self.obs_dict["day_price_30m_avg_percentage"] = np.mean(self.price_percentage_history[-30::self.dt])
 
-        self.volume_history.append(self.obs["current_volume"])
-        self.volume_percentage_history.append(self.obs["current_volume"] / self.volume_history[self.timestep-1] - 1)
+        self.volume_history.append(self.obs_dict["current_volume"])
+        self.volume_percentage_history.append(self.obs_dict["current_volume"] / self.volume_history[self.timestep-1] - 1)
 
         # Intraday volume open
-        if self.timestep==0: self.obs["day_volume_open"] = self.obs["current_volume"]
+        if self.timestep==0: self.obs_dict["day_volume_open"] = self.obs_dict["current_volume"]
         # Intraday high
-        self.obs["day_volume_high"] = max(self.obs["current_volume"], self.obs["day_volume_high"])
+        self.obs_dict["day_volume_high"] = max(self.obs_dict["current_volume"], self.obs_dict["day_volume_high"])
         # Intraday low
-        self.obs["day_volume_low"] = min(self.obs["current_volume"], self.obs["day_volume_low"])
+        self.obs_dict["day_volume_low"] = min(self.obs_dict["current_volume"], self.obs_dict["day_volume_low"])
         # Intraday open
-        self.obs["day_volume_avg"] = np.median(self.volume_history[::self.dt])
+        self.obs_dict["day_volume_avg"] = np.median(self.volume_history[::self.dt])
         # Intraday open
-        self.obs["day_volume_30m_avg"] = np.mean(self.volume_history[-30::self.dt])
+        self.obs_dict["day_volume_30m_avg"] = np.mean(self.volume_history[-30::self.dt])
 
         # Intraday price percentage change
-        self.obs["day_volume_percentage_change"]  = self.obs["current_volume"] / self.obs["day_volume_open"] - 1
+        self.obs_dict["day_volume_percentage_change"] = self.obs_dict["current_volume"] / self.obs_dict["day_volume_open"] - 1
         # Intraday price percentage high
-        self.obs["day_volume_percentage_high"]    = self.obs["current_volume"] / self.obs["day_volume_high"] - 1
+        self.obs_dict["day_volume_percentage_high"] = self.obs_dict["current_volume"] / self.obs_dict["day_volume_high"] - 1
         # Intraday price percentage low
-        self.obs["day_volume_percentage_low"]     = self.obs["current_volume"] / self.obs["day_volume_low"] - 1
+        self.obs_dict["day_volume_percentage_low"] = self.obs_dict["current_volume"] / self.obs_dict["day_volume_low"] - 1
         # Intraday price avg
-        self.obs["day_volume_median_percentage"]  = np.median(self.volume_percentage_history[::self.dt])
+        self.obs_dict["day_volume_median_percentage"] = np.median(self.volume_percentage_history[::self.dt])
         # Intraday price 30m moving avg
-        self.obs["day_volume_30m_avg_percentage"] = np.mean(self.volume_percentage_history[-30::self.dt])
+        self.obs_dict["day_volume_30m_avg_percentage"] = np.mean(self.volume_percentage_history[-30::self.dt])
 
+
+        self.obs_values = np.fromiter(self.obs_dict.values(), dtype=float)
+        if self.timestep==0:
+            self.norm_constants = np.abs([val if val > 1 else 1 for val in self.obs_values])
 
     def get_state(self):
-        return self.obs
+        return self.obs_values
 
 
 